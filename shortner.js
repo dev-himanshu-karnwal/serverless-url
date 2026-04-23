e4579417780430d8d0b36dcadea768f4b12239b2
Himanshu Karnwal <himanshukar1810@gmail.com>
2026-04-23 17:20:02 +0530

    first commit

---
+++ shortner.js
@@ -0,0 +1,53 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({ region: "ap-south-1" });
+const docClient = DynamoDBDocumentClient.from(client);
+
+function generateShortUrl() {
+  return Math.random().toString(36).substring(2, 8).toLowerCase();
+}
+
+exports.shortenUrl = async (event) => {
+  const { url } = JSON.parse(event.body);
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "URL is required" }),
+    };
+  }
+
+  const shortUrl = generateShortUrl();
+
+  const item = {
+    id: shortUrl,
+    originalUrl: url,
+    createdAt: Date.now(),
+    expiresAt: Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60, // 7 days TTL
+    clicks: 0,
+  };
+
+  try {
+    await docClient.send(
+      new PutCommand({
+        TableName: "url-shortener",
+        Item: item,
+      }),
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        shortUrl,
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not create short URL" }),
+    };
+  }
+};
